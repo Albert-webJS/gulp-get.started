9e0bdac3a3ec392fc14920944261e8ad882422f7
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-01-15 15:35:00 +0200

    created main task for project

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@
 const { src, dest } = require("gulp");
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify = require("gulp-gulpcssbeautify");
+const cssbeautify = require("gulp-cssbeautify");
 const removeComments = require("gulp-strip-css-comments");
 const rename = require("gulp-rename");
+const rigger = require("gulp-rigger");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
 const uglify = require("gulp-uglify");
@@ -17,7 +18,7 @@ const browserSync = require("browser-sync").create();
 
 // TODO created paths to build the project
 
-const src = "src";
+const srcPath = "src";
 const dist = "dist";
 
 const path = {
@@ -29,18 +30,99 @@ const path = {
     fonts: `${dist}/app/fonts/`,
   },
   src: {
-    html: `${src}/*.html`,
-    css: `${src}/app/scss/*.scss`,
-    js: `${src}/app/js/*.js`,
-    images: `${src}/app/images/**/*.{jpeg, png, svg, gif, ico}`,
-    fonts: `${src}/app/fonts/**/*.{eot, woff, woff2, ttf, svg}`,
+    html: `${srcPath}/*.html`,
+    css: `${srcPath}/app/scss/*.scss`,
+    js: `${srcPath}/app/js/**/*.js`,
+    images: `${srcPath}/app/images/**/*.{jpeg, png, svg, gif, ico}`,
+    fonts: `${srcPath}/app/fonts/**/*.{eot, woff, woff2, ttf, svg}`,
   },
   watch: {
-    html: `${src}/**/*.html`,
-    css: `${src}/app/scss/**/*.scss`,
-    js: `${src}/app/js/**/*.js`,
-    images: `${src}/app/images/**/*.{jpeg, png, svg, gif, ico}`,
-    fonts: `${src}/app/fonts/**/*.{eot, woff, woff2, ttf, svg}`,
+    html: `${srcPath}/**/*.html`,
+    css: `${srcPath}/app/scss/**/*.scss`,
+    js: `${srcPath}/app/js/**/*.js`,
+    images: `${srcPath}/app/images/**/*.{jpeg, png, svg, gif, ico}`,
+    fonts: `${srcPath}/app/fonts/**/*.{eot, woff, woff2, ttf, svg}`,
   },
   clean: `./${dist}/`,
 };
+
+function html() {
+  return src(path.src.html, { base: `${srcPath}/` })
+    .pipe(plumber())
+    .pipe(dest(path.build.html));
+}
+
+function css() {
+  return src(path.src.css, { base: `${srcPath}/app/scss/` })
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(
+      cssnano({
+        zindex: false,
+        discardComments: {
+          removeAll: true,
+        },
+      })
+    )
+    .pipe(removeComments())
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".css",
+      })
+    )
+    .pipe(dest(path.build.css));
+}
+
+function js() {
+  return src(path.src.js, { base: `${srcPath}/app/js/` })
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".js",
+      })
+    )
+    .pipe(dest(path.build.js));
+}
+
+function images() {
+  return src(path.src.images, { base: `${srcPath}/app/images/` })
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 90, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest(path.build.images));
+}
+
+function fonts() {
+  return src(path.src.fonts, { base: `${srcPath}/app/fonts/` }).pipe(
+    dest(path.build.fonts)
+  );
+}
+
+function clean() {
+  return del(path.clean);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
+
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
+exports.build = build;
---
+++ src/app/js/main.js
@@ -0,0 +1,1 @@
+//= components/logger.js
