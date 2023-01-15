e2943888989713d0e1039816fb453ccea432d278
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-01-15 11:14:52 +0200

    entered path for project

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,33 @@ const panini = require("panini");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
 const browserSync = require("browser-sync").create();
+
+// TODO created paths to build the project
+
+const src = "src";
+const dist = "dist";
+
+const path = {
+  build: {
+    html: `${dist}/`,
+    css: `${dist}/app/css/`,
+    js: `${dist}/app/js/`,
+    images: `${dist}/app/images/`,
+    fonts: `${dist}/app/fonts/`,
+  },
+  src: {
+    html: `${src}/*.html`,
+    css: `${src}/app/scss/*.scss`,
+    js: `${src}/app/js/*.js`,
+    images: `${src}/app/images/**/*.{jpeg, png, svg, gif, ico}`,
+    fonts: `${src}/app/fonts/**/*.{eot, woff, woff2, ttf, svg}`,
+  },
+  watch: {
+    html: `${src}/**/*.html`,
+    css: `${src}/app/scss/**/*.scss`,
+    js: `${src}/app/js/**/*.js`,
+    images: `${src}/app/images/**/*.{jpeg, png, svg, gif, ico}`,
+    fonts: `${src}/app/fonts/**/*.{eot, woff, woff2, ttf, svg}`,
+  },
+  clean: `./${dist}/`,
+};
